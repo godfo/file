9e04b9b8390b7f054b88507bea273cbe864e0028
出久 <53359937+LvGuChuJiu@users.noreply.github.com>
2020-01-18 22:19:54 +0800

    Update rrtv.js

--- rrtv.js
+++ rrtv.js
@@ -2,7 +2,8 @@
 Surge:
 
 [Script]
-http-response https:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll) script-path= https://raw.githubusercontent.com/LvGuChuJiu/SurgeScript/master/rrtv.js
+
+http-response https:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/LvGuChuJiu/SurgeScript/master/rrtv.js
 
 [MITM]
 hostname = *.rr.tv
