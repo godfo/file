ecc0aa3e9d2d9f4b58a835835c99a43091a4eb56
出久 <53359937+HelSingV@users.noreply.github.com>
2020-02-16 00:55:49 +0800

    Update elemSign.js

--- Scripts/Quantumult X/elemSign.js
+++ Scripts/Quantumult X/elemSign.js
@@ -1,40 +1,25 @@
 /*
 参考chavyleung和NobyDa的写法
-
 //饿了么
-
 > 代码已同时兼容 Surge & QuanX, 使用同一份签到脚本即可
-
-
 ## 配置 (Surge)
-
 ```properties
 [MITM]
 h5.ele.me
-
 [Script]
 http-request ^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ script-path=https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/elemGetCookies.js
 cron "0 5 0 * * *" script-path=https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/elemSign.js
 ```
-
 ## 配置 (QuanX)
-
 ```properties
 [MITM]
 h5.ele.me
-
 [rewrite_local]
-
 ^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ url script-request-header nzw9314/Script/elemGetCookies.js
-
-
-
 [task_local]
 5 0 * * * elemSign.js
 ```
-
 ## 说明
-
 1. 先把h5.ele.me`加到`[MITM]`
 2. 再配置重写规则:
    - Surge: 把两条远程脚本放到`[Script]`
@@ -42,9 +27,7 @@ h5.ele.me
 3. 打开 APP, 访问下`我的`>`签到领红包`(左上角)
 4. 系统提示: `获取Cookie: 成功` （如果不提示获取成功, 尝试杀进程再进`个人`>`每日签到`）
 5. 最后就可以把第 1 条脚本注释掉了
-
 > 第 1 条脚本是用来获取 cookie 的, 用浏览器访问一次获取 cookie 成功后就可以删掉或注释掉了, 但请确保在`登录成功`后再获取 cookie.
-
 > 第 2 条脚本是签到脚本, 每天`00:05:00`执行一次.
 */
 
@@ -78,13 +61,13 @@ function sign() {
     const title = `${cookieName}`
     let subTitle = ''
     let detail = ''
-    sy.log(response);
-    if (response == 200) {
+    sy.log(response.status);
+    if (response.status == 200) {
       subTitle = '签到结果: 成功'
       // else subTitle = '签到结果: 成功 (重复签到)'
       // detail = `人人钻: ${result.data.userinfo.point}, 登录天数: ${result.data.usercount.cont_login} -> ${result.data.upgrade_day}`
      sy.msg(title, subTitle, detail)
-    } else if(response == 400) {
+    } else if(response.status == 400) {
       subTitle = '签到结果: 重复'
       sy.msg(title, subTitle, detail)
     }
@@ -127,15 +110,17 @@ function init() {
       $task.fetch(url).then((resp) => cb(null, {}, resp.body))
     }
   }
-  post = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.post(url, cb)
-    }
+  post = (options, callback) => {
     if (isQuanX()) {
-      url.method = 'POST'
-      $task.fetch(url).then((resp) => cb(null, resp.statusCode, resp.body))
+        if (typeof options == "string") options = { url: options }
+        options["method"] = "POST"
+        $task.fetch(options).then(response => {
+            response["status"] = response.statusCode
+            callback(null, response, response.body)
+        }, reason => callback(reason.error, null, null))
     }
-  }
+    if (isSurge()) $httpClient.post(options, callback)
+}
   done = (value = {}) => {
     $done(value)
   }
