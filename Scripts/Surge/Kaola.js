cc01f7af516fb513725f982820b8ca1c5ed0ce25
出久 <53359937+HelSingV@users.noreply.github.com>
2020-02-17 14:10:45 +0800

    Update Kaola.js

--- Scripts/Surge/Kaola.js
+++ Scripts/Surge/Kaola.js
@@ -1,6 +1,6 @@
 /*
 [Script]
-http-request ^https://sp\.kaola\.com/api/openad$ script-path=https://Choler.github.io/Surge/Script/Kaola.js
+http-request ^https://sp\.kaola\.com/api/openad$ script-path=https://raw.githubusercontent.com/HelSingV/file/master/Scripts/Surge/Kaola.js
 
 [MITM]
 hostname = sp.kaola.com
